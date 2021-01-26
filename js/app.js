967e3f5caf0090eb46005cc806bcb055dba73441
ivan-user <username.web@protonmail.com>
2021-01-26 18:45:27 +0100

    UI improvements

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-
-const shoot = document.querySelector('.shoot');
-shoot.addEventListener('click', (e) => {
-    console.log('clicked');
-})
-
 // shoot.addEventListener('mousemove', (e) => {
 //     let log = document.querySelector('#log');
 //     log.innerText = `Screen X/Y: ${e.screenX}, ${e.screenY}`
@@ -17,28 +11,28 @@ bugs.addEventListener('click', () => {
     increaseScore();
 })
 
-const bugArr = new Array('/assets/worm1.png', '/assets/bug1.png', '/assets/spider1.png')
+const bugArr = new Array('/assets/worm1.png', '/assets/bug1.png', '/assets/spider1.png', '/assets/centipede1.png')
 function randomImage() {
     let randomBug = Math.floor(Math.random() * bugArr.length);
     document.querySelector('#bug').src = bugArr[randomBug];
 }
 
 const moveBug = () => {
-    const topCords = randomPos(650);
-    const leftCords = randomPos(800);
+    let topCords = randomPos(600);
+    let leftCords = randomPos(8000);
     bugs.style.top = `${topCords}px`;
     bugs.style.left = `${leftCords}px`;
 }
 
 const increaseScore = () => {
-    const score = document.querySelector("#score-counter").innerHTML;
-    let count = score;
-    console.log('Score: ',count);
-    score.innerHTML = count + 1;
+    const score = document.querySelector(".scoreContainer #score-counter span").innerHTML;
+    let count = 0;
+    score.innerHTML = count++;
+    console.log('Count: ', count, 'Score: ',score);
 }
 
-const randomPos = (num) => {
-    let randomNum = Math.floor(Math.random() * Math.floor(num));
+const randomPos = () => {
+    let randomNum = Math.floor(Math.random() * 1000);
     console.log('random position: ',randomNum);
     return randomNum;
 }
