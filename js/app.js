e3a11fd88320fcbedb180641720cc0b6bfcc96ee
ivan-user <username.web@protonmail.com>
2021-01-28 21:13:14 +0100

    Finish game, some bugs left

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ startBtn.addEventListener('click', () => {
     modal.style.display = "none";
     overlay.style.display = "none";
     countDown();
-})
+});
 
 
 // mouse click on the image of the bug should call 3 functions:
@@ -14,7 +14,8 @@ startBtn.addEventListener('click', () => {
 const bugs = document.querySelector('#bugDiv');
 bugs.addEventListener('click', () => {
     moveBug();
-    randomImage();    
+    setInterval(moveBug, 3000);
+    setInterval(randomImage, 3000);
     increaseScore();
 })
 
@@ -24,7 +25,6 @@ const randomImage = () => {
     let randomBug = Math.floor(Math.random() * bugArr.length);
     document.querySelector('.bugs').src = bugArr[randomBug];
 }
-
 
 // moveBug() should move the image of the bug to a random position
 // on the area of the screen
@@ -51,35 +51,24 @@ const randomPos = () => {
 
 // timer for the game
 let counter = 0;
-let timeLeft = 10;
+let timeLeft = 30;
 const countDown = () => {
     let timer = document.querySelector('#timer');
-    timer.innerHTML = `${timeLeft - counter}`;
-    let randomTime = setInterval(randomImage, 1000);
-
     const interval = setInterval(timeIt, 1000);
-
     function timeIt() {
         counter ++;
         timer.innerHTML = `${timeLeft - counter}`;
     
-        if(counter == timeLeft || score > 30) {
+        if(counter == timeLeft && score > 15) {
             counter = 0;
-            console.log('win')
+            window.open("notDead.html");
             clearInterval(interval);
             location.reload();
-            gameWin();
-        }
-         else if (counter == timeLeft || score > 30){
+        }else{
             counter = 0;
+            window.open("dead.html");
             clearInterval(interval);
-            gameLose();
+            location.reload();
         }
     }
-}
-const gameWin = () => {
-    window.open("../assets/winVid.mp4")
-}
-const gameLose = () => {
-    window.open("")
 }
