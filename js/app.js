4e5638d70273c4a0dfb78611b5243e6921b1415c
ivan-user <username.web@protonmail.com>
2021-01-29 09:19:41 +0100

    Finished finished

--- js/app.js
+++ js/app.js
@@ -9,22 +9,22 @@ startBtn.addEventListener('click', () => {
 });
 
 
-// mouse click on the image of the bug should call 3 functions:
-// moveBug(), randomImage(), increaseScore()
-const bugs = document.querySelector('#bugDiv');
-bugs.addEventListener('click', () => {
-    moveBug();
-    setInterval(moveBug, 3000);
-    setInterval(randomImage, 3000);
-    increaseScore();
-})
-
 //radnomImage() generates a random image from the array
 const bugArr = new Array('../assets/bug1.png', '../assets/bug2.png', '../assets/bug3.png', '../assets/bug4.png')
 const randomImage = () => {
     let randomBug = Math.floor(Math.random() * bugArr.length);
     document.querySelector('.bugs').src = bugArr[randomBug];
 }
+
+// mouse click on the image of the bug should call 3 functions:
+// moveBug(), randomImage(), increaseScore()
+const bugs = document.querySelector('#bugDiv');
+bugs.addEventListener('click', () => {
+    moveBug();
+    setInterval(moveBug, 3000);
+    randomImage();
+    increaseScore();
+})
 
 // moveBug() should move the image of the bug to a random position
 // on the area of the screen
@@ -53,18 +53,20 @@ const randomPos = () => {
 let counter = 0;
 let timeLeft = 30;
 const countDown = () => {
+
     let timer = document.querySelector('#timer');
     const interval = setInterval(timeIt, 1000);
+
     function timeIt() {
         counter ++;
         timer.innerHTML = `${timeLeft - counter}`;
     
-        if(counter == timeLeft && score > 15) {
+        if(counter == timeLeft || score > 15) {
             counter = 0;
             window.open("notDead.html");
             clearInterval(interval);
             location.reload();
-        }else{
+        }else if (counter == timeLeft || score < 15){
             counter = 0;
             window.open("dead.html");
             clearInterval(interval);
